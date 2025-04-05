76ae315ad7b99c850b9cf48fab518018d8ca22e2
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-04-04 21:02:40 -0400

    Catppuccin-latte for neovim

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -2,15 +2,21 @@ return {
   -- "folke/tokyonight.nvim",
   -- opts = { style = "night" },
 
-  "maxmx03/solarized.nvim",
-  lazy = false,
-  priority = 1000,
-  ---@type solarized.config
-  opts = {},
-  config = function(_, opts)
-    vim.o.termguicolors = true
-    vim.o.background = "light"
-    require("solarized").setup(opts)
-    vim.cmd.colorscheme("solarized")
-  end,
+  -- "maxmx03/solarized.nvim",
+  -- lazy = false,
+  -- priority = 1000,
+  -- ---@type solarized.config
+  -- opts = {},
+  -- config = function(_, opts)
+  --   vim.o.termguicolors = true
+  --   vim.o.background = "light"
+  --   require("solarized").setup(opts)
+  --   vim.cmd.colorscheme("solarized")
+  -- end,
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-latte",
+    },
+  },
 }
