123505e15eaad781cec879efec26860a1673e7e0
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-06-02 16:19:09 -0400

    Update neovim with zk, hardtime, some new keymaps

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = vim.fn.expand("~/notes") .. "/*",
+  callback = function()
+    vim.opt_local.textwidth = 80
+    vim.opt_local.formatoptions:append("t")
+  end,
+})
