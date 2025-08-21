1ed00a71b1b4cea3b15f5c6d2ea4124420873921
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-08-20 21:31:26 -0400

    nvim: update + tokyonight-night

--- nvim/.config/nvim/lua/plugins/color.lua
+++ nvim/.config/nvim/lua/plugins/color.lua
@@ -1,22 +1,8 @@
 return {
-  -- "folke/tokyonight.nvim",
-  -- opts = { style = "night" },
-
-  -- "maxmx03/solarized.nvim",
-  -- lazy = false,
-  -- priority = 1000,
-  -- ---@type solarized.config
-  -- opts = {},
-  -- config = function(_, opts)
-  --   vim.o.termguicolors = true
-  --   vim.o.background = "light"
-  --   require("solarized").setup(opts)
-  --   vim.cmd.colorscheme("solarized")
-  -- end,
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "tokyonight-night",
     },
   },
 }
