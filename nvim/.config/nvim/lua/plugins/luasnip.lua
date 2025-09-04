756da4e6dc9d2d30918b53b89b6dba0982ec2527
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-09-04 07:33:21 -0400

    nvim: luasnip - i_C-O

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -15,11 +15,11 @@ return {
 			ls.jump(-1)
 		end, { silent = true })
 
-		vim.keymap.set({ "i", "s" }, "<C-O>", function()
-			if ls.choice_active() then
-				ls.change_choice(1)
-			end
-		end, { silent = true })
+		-- vim.keymap.set({ "i", "s" }, "<C-O>", function()
+		-- 	if ls.choice_active() then
+		-- 		ls.change_choice(1)
+		-- 	end
+		-- end, { silent = true })
 		require("config.snippets")
 	end,
 }
