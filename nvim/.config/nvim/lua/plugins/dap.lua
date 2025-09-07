8d5708caa67c9b377018175e05f978d7485aa845
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-09-06 21:04:16 -0400

    nvim: nvim-dap-ui

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -1,17 +1,18 @@
 return {
-	"mfussenegger/nvim-dap",
-	recommended = true,
-	desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
+	{
+		"mfussenegger/nvim-dap",
+		recommended = true,
+		desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
 
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		-- virtual text for the debugger
-		{
-			"theHamsta/nvim-dap-virtual-text",
-			opts = {},
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			-- virtual text for the debugger
+			{
+				"theHamsta/nvim-dap-virtual-text",
+				opts = {},
+			},
+			"mfussenegger/nvim-dap-python",
 		},
-		"mfussenegger/nvim-dap-python",
-	},
 
   -- stylua: ignore
   keys = {
@@ -34,8 +35,33 @@ return {
     { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
   },
 
-	config = function()
-		require("dap-python").setup()
-		vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
-	end,
+		config = function()
+			require("dap-python").setup()
+			vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "nvim-neotest/nvim-nio" },
+    -- stylua: ignore
+    keys = {
+      { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+      { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+    },
+		opts = {},
+		config = function(_, opts)
+			local dap = require("dap")
+			local dapui = require("dapui")
+			dapui.setup(opts)
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open({})
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close({})
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close({})
+			end
+		end,
+	},
 }
