8a389a21012d3bdaa2d20acc47e80b4e3c3d1de5
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-09-11 07:25:47 -0400

    nvim: improved lsp config?

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,4 +1,10 @@
-local servers = { "lua_ls", "pyright", "ast_grep", "ts_ls", "marksman" }
+local servers = { "lua_ls", "pyright", "pyrefly", "ast_grep", "ts_ls", "marksman", "stylua" }
+
+vim.lsp.config["pyright"] = require("plugins.lspconfigs.pyright")
+vim.lsp.config["pyrefly"] = require("plugins.lspconfigs.pyrefly")
+vim.lsp.enable("pyrefly")
+
+vim.diagnostic.config({ virtual_text = true })
 
 return {
 	{
@@ -14,49 +20,5 @@ return {
 				ensure_installed = servers,
 			})
 		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function(_, opts)
-			vim.diagnostic.config(opts.diagnostics)
-
-			local lspconfig = require("lspconfig")
-			local keymaps = require("config.lsp_keymaps")
-
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-			for _, server in ipairs(servers) do
-				local server_opts = {
-					on_attach = keymaps.on_attach,
-					capabilities = capabilities,
-				}
-				lspconfig[server].setup(server_opts)
-			end
-		end,
-		opts = {
-			diagnostics = {
-				underline = true,
-				virtual_text = {
-					spacing = 2,
-					source = "if_many",
-					prefix = ">",
-				},
-				update_in_insert = true,
-				signs = {
-					text = {
-						[vim.diagnostic.severity.ERROR] = "",
-						[vim.diagnostic.severity.WARN] = "",
-						[vim.diagnostic.severity.HINT] = "",
-						[vim.diagnostic.severity.INFO] = "",
-					},
-					numhl = {
-						[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-						[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-						[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
-						[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
-					},
-				},
-			},
-		},
 	},
 }
