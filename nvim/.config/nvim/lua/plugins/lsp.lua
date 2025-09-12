047919eb5d7c8e2dea5716b64d6e21a9ef594a7c
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-09-12 06:56:40 -0400

    nvim: diagnostics, servers

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,15 +1,49 @@
 -- mason-tool-installer only accepts mason names
-local servers =
-	{ "lua-language-server", "pyright", "pyrefly", "ast-grep", "typescript-language-server", "marksman", "stylua" }
+local servers = {
+	"lua-language-server",
+	"pyright",
+	"pyrefly",
+	"ast-grep",
+	"typescript-language-server",
+	"marksman",
+	"stylua",
+	"ruff",
+}
 
 -- configs will automatically be read from lsp/. use these names
 vim.lsp.enable("pyrefly")
+vim.lsp.enable("ruff")
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("ast_grep")
 vim.lsp.enable("ts_ls")
 vim.lsp.enable("marksman")
 
-vim.diagnostic.config({ virtual_text = true })
+vim.diagnostic.config({
+	severity_sort = true,
+	float = { border = "rounded", source = true },
+	underline = { severity = vim.diagnostic.severity.ERROR },
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "󰅚 ",
+			[vim.diagnostic.severity.WARN] = "󰀪 ",
+			[vim.diagnostic.severity.INFO] = "󰋽 ",
+			[vim.diagnostic.severity.HINT] = "󰌶 ",
+		},
+	},
+	virtual_text = {
+		source = "if_many",
+		spacing = 2,
+		format = function(diagnostic)
+			local diagnostic_message = {
+				[vim.diagnostic.severity.ERROR] = diagnostic.message,
+				[vim.diagnostic.severity.WARN] = diagnostic.message,
+				[vim.diagnostic.severity.INFO] = diagnostic.message,
+				[vim.diagnostic.severity.HINT] = diagnostic.message,
+			}
+			return diagnostic_message[diagnostic.severity]
+		end,
+	},
+})
 
 return {
 	{
