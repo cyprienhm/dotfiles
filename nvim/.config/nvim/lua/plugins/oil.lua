3822b5efa8f5b8bca556e5e00ad965b0ed4ea7ac
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-09-15 07:53:30 -0400

    nvim: oil maps + cols

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -2,7 +2,15 @@ return {
 	"stevearc/oil.nvim",
 	---@module 'oil'
 	---@type oil.SetupOpts
-	opts = {},
+	opts = {
+		default_file_explorer = true,
+		columns = { "icon", "size", "mtime" },
+		keymaps = {
+			["<C-v>"] = { "actions.select", opts = { vertical = true } },
+			["<C-s>"] = { "actions.select", opts = { horizontal = true } },
+			["<C-h>"] = {},
+		},
+	},
 	-- Optional dependencies
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
