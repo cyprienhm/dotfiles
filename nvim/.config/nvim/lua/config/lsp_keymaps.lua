de6b9f8476a0c8c0e857875a140152b6afe01934
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-10-09 07:49:01 -0400

    nvim: less snacks dependency for lsp

--- nvim/.config/nvim/lua/config/lsp_keymaps.lua
+++ nvim/.config/nvim/lua/config/lsp_keymaps.lua
@@ -8,16 +8,7 @@ function M.get()
 	end
   -- stylua: ignore
 	M._keys = {
-		{ "gd", Snacks.picker.lsp_definitions, desc = "Goto Definition" },
-		{ "grr", Snacks.picker.lsp_references, desc = "References", nowait = true },
-		{ "gI", Snacks.picker.lsp_implementations, desc = "Goto Implementation" },
-		{ "gy", Snacks.picker.lsp_type_definitions, desc = "Goto T[y]pe Definition" },
-		{ "gD", Snacks.picker.lsp_declarations, desc = "Goto Declaration" },
-		{ "gK", function() return vim.lsp.buf.signature_help() end, desc = "Signature Help", has = "signatureHelp", },
-		{ "<leader>cc", vim.lsp.codelens.run, desc = "Run Codelens", mode = { "n", "v" }, has = "codeLens" },
-		{ "<leader>cC", vim.lsp.codelens.refresh, desc = "Refresh & Display Codelens", mode = { "n" }, has = "codeLens", },
 		{ "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File", mode = { "n" }, has = { "workspace/didRenameFiles", "workspace/willRenameFiles" }, },
-		{ "<leader>cr", vim.lsp.buf.rename, desc = "Rename", has = "rename" },
 	}
 	return M._keys
 end
