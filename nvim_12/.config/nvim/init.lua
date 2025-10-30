bff207e4265a4b7ac8574991e451b4a56c0d5026
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2025-10-29 21:42:57 -0400

    nvim 0.12: remove useless format for diagnostics

--- nvim_12/.config/nvim/init.lua
+++ nvim_12/.config/nvim/init.lua
@@ -234,15 +234,6 @@ vim.diagnostic.config({
 	virtual_text = {
 		source = true,
 		spacing = 2,
-		format = function(diagnostic)
-			local diagnostic_message = {
-				[vim.diagnostic.severity.ERROR] = diagnostic.message,
-				[vim.diagnostic.severity.WARN] = diagnostic.message,
-				[vim.diagnostic.severity.INFO] = diagnostic.message,
-				[vim.diagnostic.severity.HINT] = diagnostic.message,
-			}
-			return diagnostic_message[diagnostic.severity]
-		end,
 	},
 })
 
