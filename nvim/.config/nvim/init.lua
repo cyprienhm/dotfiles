279bcd45c25236df9833f183917a225dac18398e
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2026-02-04 22:46:15 -0500

    nvim: slime_dont_ask_default = 1

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -899,6 +899,7 @@ require("marks").setup()
 vim.cmd('let g:slime_target = "tmux"')
 vim.cmd('let g:slime_default_config = {"socket_name": "default", "target_pane": "{last}"}')
 vim.cmd("let g:slime_bracketed_paste = 1")
+vim.cmd("let g:slime_dont_ask_default = 1")
 
 -- surround
 require("mini.surround").setup({
