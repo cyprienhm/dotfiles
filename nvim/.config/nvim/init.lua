bc2e15edb3ed48ab6d8dbe345c239e4aec90158b
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2026-03-18 18:20:10 -0400

    nvim: enable documentation and signature for blink.cmp

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -859,12 +859,11 @@ require("blink.cmp").setup({
 		nerd_font_variant = "mono",
 	},
 
-	-- (Default) Only show the documentation popup when manually triggered
-	completion = { documentation = { auto_show = false } },
+	completion = { documentation = { auto_show = true } },
+	signature = { enabled = true },
 
 	snippets = { preset = "luasnip" },
-	-- Default list of enabled providers defined so that you can extend it
-	-- elsewhere in your config, without redefining it, due to `opts_extend`
+
 	sources = {
 		default = { "lsp", "path", "snippets", "buffer" },
 	},
