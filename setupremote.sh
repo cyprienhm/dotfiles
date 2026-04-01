72a7a1e1dbb89e00c723f50b427cd93548a47514
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2026-03-31 22:52:38 -0400

    setupremote: add .bashrc

--- setupremote.sh
+++ setupremote.sh
@@ -9,6 +9,7 @@ git clone --depth 1 "$REPO" "$TMP"
 
 cp "$TMP/vim/.vimrc" "$HOME/.vimrc"
 cp "$TMP/tmux/.tmux.conf.remote" "$HOME/.tmux.conf"
+cp "$TMP/bash/.bashrc" "$HOME/.bashrc"
 
 rm -rf "$HOME/.config/nvim"
 mkdir -p "$HOME/.config/nvim"
