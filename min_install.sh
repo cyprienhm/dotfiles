4938d4d667fdc2f34fddcfaf26b4251cf2d57c2d
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2026-03-31 22:48:54 -0400

    replace min_install.sh with setupremote.sh

--- min_install.sh
+++
@@ -1,3 +0,0 @@
-ln -s "$PWD/vim/.vimrc" "$HOME/.vimrc"
-ln -s "$PWD/nvim/.config/nvim" "$HOME/.config/nvim"
-ln -s "$PWD/tmux/.tmux.conf" "$HOME/.tmux.conf"
---
+++ setupremote.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+REPO="https://github.com/cyprienhm/dotfiles.git"
+TMP="/tmp/dotfiles"
+
+rm -rf "$TMP"
+git clone --depth 1 "$REPO" "$TMP"
+
+cp "$TMP/vim/.vimrc" "$HOME/.vimrc"
+cp "$TMP/tmux/.tmux.conf.remote" "$HOME/.tmux.conf"
+
+rm -rf "$HOME/.config/nvim"
+mkdir -p "$HOME/.config/nvim"
+cp -r "$TMP/nvim/.config/nvim/." "$HOME/.config/nvim/"
+
+rm -rf "$TMP"
