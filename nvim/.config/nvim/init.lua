bb8f1f93720c19a8935436153ce3b61309964259
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2026-04-20 23:18:03 -0400

    nvim: ui2, difftool

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -169,6 +169,8 @@ opt.fillchars = {
 
 -- plugins
 
+vim.cmd("packadd nvim.difftool")
+
 -- hooks for plugins which need them
 -- taken from :h vim.pack-events
 local plugins_hooks = function(ev)
@@ -1077,4 +1079,29 @@ require("tableview").setup({
 	max_rows = 100,
 	column_width_cap = 50,
 	auto_open = { "parquet", "arrow", "feather", "xlsx" },
+})
+
+-- ui2
+require("vim._core.ui2").enable({
+	enable = true, -- Whether to enable or disable the UI.
+	msg = { -- Options related to the message module.
+		---@type 'cmd'|'msg' Default message target, either in the
+		---cmdline or in a separate ephemeral message window.
+		---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target
+		---or table mapping |ui-messages| kinds and triggers to a target.
+		targets = "cmd",
+		cmd = { -- Options related to messages in the cmdline window.
+			height = 0.5, -- Maximum height while expanded for messages beyond 'cmdheight'.
+		},
+		dialog = { -- Options related to dialog window.
+			height = 0.5, -- Maximum height.
+		},
+		msg = { -- Options related to msg window.
+			height = 0.5, -- Maximum height.
+			timeout = 4000, -- Time a message is visible in the message window.
+		},
+		pager = { -- Options related to message window.
+			height = 1, -- Maximum height.
+		},
+	},
 })
