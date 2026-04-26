f61a19cc07394ccfbe3925afdd041265a8958d18
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2026-04-26 12:11:22 -0400

    nvim: update blink to v2, add blink.lib. call setup for typst-preview. update vim.pack lock

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -221,6 +221,7 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
 	{ src = "https://github.com/lukas-reineke/indent-blankline.nvim", name = "ibl" },
 	{ src = "https://github.com/saghen/blink.cmp" },
+	{ src = "https://github.com/saghen/blink.lib" },
 	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
 	{ src = "https://github.com/Bekaboo/dropbar.nvim" },
 	{ src = "https://github.com/chentoast/marks.nvim" },
@@ -900,27 +901,15 @@ end)
 require("ibl").setup({ indent = { highlight = highlight } })
 
 -- blink
+require("blink.cmp").build():wait(60000)
 require("blink.cmp").setup({
-	-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-	-- 'super-tab' for mappings similar to vscode (tab to accept)
-	-- 'enter' for enter to accept
-	-- 'none' for no mappings
-	--
-	-- All presets have the following mappings:
-	-- C-space: Open menu or open docs if already open
-	-- C-n/C-p or Up/Down: Select next/previous item
-	-- C-e: Hide menu
-	-- C-k: Toggle signature help (if signature.enabled = true)
-	--
-	-- See :h blink-cmp-config-keymap for defining your own keymap
+	-- :h blink-cmp-config-keymap
 	keymap = {
 		preset = "default",
 		["<C-k>"] = false,
 	},
 
 	appearance = {
-		-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-		-- Adjusts spacing to ensure icons are aligned
 		nerd_font_variant = "mono",
 	},
 
@@ -933,12 +922,7 @@ require("blink.cmp").setup({
 		default = { "lsp", "path", "snippets", "buffer" },
 	},
 
-	-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-	-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-	-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-	--
-	-- See the fuzzy documentation for more information
-	fuzzy = { implementation = "prefer_rust_with_warning", prebuilt_binaries = { force_version = "v1.7.0" } },
+	fuzzy = { implementation = "rust" },
 })
 
 -- gitsigns
@@ -1150,3 +1134,6 @@ require("vim._core.ui2").enable({
 		},
 	},
 })
+
+-- typst preview
+require("typst-preview").setup()
