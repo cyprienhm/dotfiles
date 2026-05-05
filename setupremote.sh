f78e7030067db09e47ed26ca5321b073aa66ac00
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2026-05-05 10:30:56 -0400

    remote: use a marker

--- setupremote.sh
+++ setupremote.sh
@@ -9,7 +9,9 @@ git clone --depth 1 "$REPO" "$TMP"
 
 cp "$TMP/vim/.vimrc" "$HOME/.vimrc"
 cp "$TMP/tmux/.tmux.conf.remote" "$HOME/.tmux.conf"
-cp "$TMP/bash/.bashrc.remote" "$HOME/.bashrc"
+if ! grep -qF "# >>> dotfiles bashrc >>>" "$HOME/.bashrc" 2>/dev/null; then
+    cat "$TMP/bash/.bashrc.remote" >> "$HOME/.bashrc"
+fi
 
 rm -rf "$HOME/.config/nvim"
 mkdir -p "$HOME/.config/nvim"
