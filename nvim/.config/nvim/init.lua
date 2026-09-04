451a399b147ee8bbf0152a42285799b8f3d1b70b
Cyprien <151447198+cyprienhm@users.noreply.github.com>
2026-09-03 20:42:54 -0400

    nvim: on_yank->hl_op

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -137,7 +137,7 @@ end, { desc = "Yank relative path" })
 vim.api.nvim_create_autocmd("TextYankPost", {
 	group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
 	callback = function()
-		vim.hl.on_yank()
+		vim.hl.hl_op()
 	end,
 })
 
